06ca9e1aa31bea9fb461b918b7c7dcde27d35314
RealArtisanFoods <105236007+RealArtisanFoods@users.noreply.github.com>
2023-03-17 21:08:59 -0500

    Add most functionality to calculator and make it purple
    Clicking an operator and then `=` is still bugged and no decimals

--- calc.js
+++ calc.js
@@ -17,6 +17,8 @@ function init() {
 function handleButtonClick(e) {
     // if number is clicked, append to display
     if (this.classList.contains('num')) {
+        // don't allow equal repeat because numbers changed
+        setEqualCanRepeat(false);
         // store previous number and input new one if operator was selected
         if (getClearOnEntry()) {
             // store previous number
@@ -26,12 +28,24 @@ function handleButtonClick(e) {
             // allow subsequent number entries to concat
             setClearOnEntry(false);
         }
-        concatDisplay(this.querySelector('h2').textContent);
+        // get text content
+        const textContent = this.querySelector('h2').textContent;
+        concatDisplay(textContent);
         return;
     }
 
+
+
     // set current operation if operator is selected
     if (['add','subtract','divide','multiply'].includes(this.id)) {
+        // do last operation if number is stored
+        if (getStoredNumber() && getCurrentOperation() && !getClearOnEntry()) {
+            // do the stored operation
+            operateStoreAndDisplay(getCurrentOperation(), getStoredNumber(), getDisplayedNumber());
+            // disallow = repetition
+            setEqualCanRepeat(false);
+        }
+
         setCurrentOperation(this.id);
         setClearOnEntry(true)
         return
@@ -40,22 +54,47 @@ function handleButtonClick(e) {
     // decide what to do if other key is clicked
     switch (this.id) {
         case '=':
-            if (getStoredNumber() === NaN) break;
-            console.log('operating');
-            console.log('current operation: ' + getCurrentOperation());
-            console.log('stored number: ' + getStoredNumber());
-            console.log('displayed number: ' + getDisplayedNumber());
-            const result = operate(getCurrentOperation(), getStoredNumber(), getDisplayedNumber());
-            console.log('result: ' + result);
-            setDisplayedNumber(result);
+            // if equal allowed to repeat, repeat it
+            if (getEqualCanRepeat()) {
+                repeatLastEval();
+                return
+            }
+            // if no number stored, do not operate
+            if (isNaN(getStoredNumber())) break;
+            operateStoreAndDisplay(getCurrentOperation(), getStoredNumber(), getDisplayedNumber())
+            // allow equal repetition
+            setEqualCanRepeat(true);
             break;
         case 'C':
             clearDisplay();
+            setDisplayedNumber(0);
             break;
         case 'CE':
             resetCalculator();
             break;
+        case 'negate':
+            negateDisplayedNumber();
     }
+}
+
+// 
+function operateStoreAndDisplay(operation, storedNumber, currentNumber) {
+    // don't do anything if there is no stored number to operate with
+    if (isNaN(getStoredNumber())) return;
+    const result = operate(operation, storedNumber, currentNumber);
+    // store evaluation for re-use if = is clicked again
+    setLastEval(operation, currentNumber);
+    setDisplayedNumber(result);
+    setStoredNumber(result);
+    setCurrentOperation('');
+    setClearOnEntry(true);
+}
+
+// retrieve last evaluation and repeat the operation
+function repeatLastEval() {
+    const lastEval = getLastEval();
+    // operate with last operation and the number pair and stored number
+    operateStoreAndDisplay(lastEval[0], getStoredNumber(), lastEval[1]);
 }
 
 // store displayed number to calculator object
@@ -78,6 +117,8 @@ function operate(operator, a, b) {
     // convert to numbers
     a = +a;
     b = +b;
+
+    console.log(`Operating ${a} ${operator} ${b}`);
     
     // select operation based on provided string
     switch (operator) {
@@ -111,12 +152,27 @@ function divide(a, b) {
 
 // get number currently displayed
 function getDisplayedNumber() {
-    return document.querySelector('#display h1').textContent;
+    // return accurate number, multiplying by -1 if negative sign is displayed
+    const number = document.querySelector('#display h1').textContent
+    return String(number * (displayingNegative() ? -1 : 1));
 }
 
 // set number to be displayed on calculator
 function setDisplayedNumber(number) {
     console.log('setting display to ' + number);
+    // if the number is negative, display negative sign and absolute value
+    const negativeSpan = document.querySelector('#display span')
+    if (number < 0) {
+        negativeSpan.textContent = '-';
+    } else {
+        negativeSpan.textContent = '';
+    }
+    // set display error if not a number, otherwise show absolute value in h1
+    if (isNaN(number)) {
+        number = 'ERROR';
+    } else {
+        number = Math.abs(number)
+    }
     document.querySelector('#display h1').textContent = number;
 }
 
@@ -156,6 +212,39 @@ function setClearOnEntry(bool) {
     calculator.clearOnEntry = bool;
 }
 
+function displayingNegative() {
+    return document.querySelector('#display span').textContent === '-';
+}
+
+// store last evalution for repeated =
+function setLastEval(operator, b) {
+    calculator.lastEval = [operator, b];
+}
+
+// retrieve last evaluation
+function getLastEval() {
+    return calculator.lastEval;
+}
+
+// set whether = can repeat
+function setEqualCanRepeat(bool) {
+    calculator.equalCanRepeat = bool;
+}
+
+// get whether = can repeat
+function getEqualCanRepeat() {
+    return calculator.equalCanRepeat;
+}
+
+// set positive to negative and vice versa
+function negateDisplayedNumber() {
+    setDisplayedNumber(+getDisplayedNumber() * -1);
+}
+
+function handleEntry(number) {
+
+}
+
 // reset entire calculator
 function resetCalculator() {
     clearDisplay();
@@ -163,7 +252,11 @@ function resetCalculator() {
     setCurrentOperation('');
     setClearOnEntry(false);
     setDisplayedNumber(0);
+    setLastEval(null);
+    setEqualCanRepeat(false);
 }
+
+
 
 // run init function on page load
 init();
