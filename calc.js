1012a77973c1a612b50d9c660ae4d0549465a264
RealArtisanFoods <105236007+RealArtisanFoods@users.noreply.github.com>
2023-03-19 17:04:54 -0500

    Implement decimal entry and floating point math

--- calc.js
+++ calc.js
@@ -53,6 +53,24 @@ function handleButtonClick(e) {
 
     // decide what to do if other key is clicked
     switch (this.id) {
+        case 'point':
+            // do nothing if number is already a float (decimal) and entry is not about to be cleared
+            if (getDisplayingFloat() && !getClearOnEntry()) return;
+            if (getClearOnEntry()) {
+                // store previous number
+                storeDisplayedNumber();
+                // clear display to make way for new number
+                clearDisplay();
+                // allow subsequent number entries to concat
+                setClearOnEntry(false);
+                // allow '.' to be pressed
+                setDisplayingFloat(false);
+            }
+            setDisplayingFloat(true);
+            // concat . on display and prevent further clicking of .
+            concatDisplay('.');
+            
+            break;
         case '=':
             // if equal allowed to repeat, repeat it
             if (getEqualCanRepeat()) {
@@ -73,7 +91,10 @@ function handleButtonClick(e) {
             resetCalculator();
             break;
         case 'negate':
+            // do nothing if display is going to reset on next entry
+            if (getClearOnEntry()) return;
             negateDisplayedNumber();
+            break;
     }
 }
 
@@ -85,8 +106,10 @@ function operateStoreAndDisplay(operation, storedNumber, currentNumber) {
     // store evaluation for re-use if = is clicked again
     setLastEval(operation, currentNumber);
     setDisplayedNumber(result);
+    // set stored number for use in subsequent operation
     setStoredNumber(result);
     setCurrentOperation('');
+    // prevent further number entry from appending to end of solution
     setClearOnEntry(true);
 }
 
@@ -102,16 +125,6 @@ function storeDisplayedNumber() {
     setStoredNumber(+getDisplayedNumber());
 }
 
-// set stored number in calculator object
-function setStoredNumber(n) {
-    calculator.stored = n;
-}
-
-
-function getStoredNumber() {
-    return calculator.stored;
-}
-
 // take operator and two numbers and return calculation
 function operate(operator, a, b) {
     // convert to numbers
@@ -119,68 +132,39 @@ function operate(operator, a, b) {
     b = +b;
 
     console.log(`Operating ${a} ${operator} ${b}`);
-    
+
+
+    // define float conversion variables in case of add or subtract
+    let intProduct, M;
+
     // select operation based on provided string
     switch (operator) {
-        case 'add': 
-            return add(a, b);
-        case 'subtract': 
-            return subtract(a, b);
         case 'multiply': 
             return multiply(a, b);
         case 'divide': 
             return divide(a, b);
+        case operator:
+            // convert to integer for addition and subtraction
+            M = 10 ** digitsAfterDecimal(b);
+            // multiply to convert to integer
+            a = a * M;
+            b = b * M;
+        // for adding and subtracting, break instead of return to allow integer conversion        
+        case 'add': 
+            intProduct = add(a, b);
+            break;
+        case 'subtract': 
+            intProduct = subtract(a, b);
+            break;
     }
+    // divide to get float result
+    return intProduct / M;
 }
-
-// add
-function add(a, b) {
-    return a + b;
-}
-// subtract
-function subtract(a, b) {
-    return a - b;
-}
-// multiply
-function multiply(a, b) {
-    return a * b;
-}
-// divide
-function divide(a, b) {
-    return a / b;
-}
-
-// get number currently displayed
-function getDisplayedNumber() {
-    // return accurate number, multiplying by -1 if negative sign is displayed
-    const number = document.querySelector('#display h1').textContent
-    return String(number * (displayingNegative() ? -1 : 1));
-}
-
-// set number to be displayed on calculator
-function setDisplayedNumber(number) {
-    console.log('setting display to ' + number);
-    // if the number is negative, display negative sign and absolute value
-    const negativeSpan = document.querySelector('#display span')
-    if (number < 0) {
-        negativeSpan.textContent = '-';
-    } else {
-        negativeSpan.textContent = '';
-    }
-    // set display error if not a number, otherwise show absolute value in h1
-    if (isNaN(number)) {
-        number = 'ERROR';
-    } else {
-        number = Math.abs(number)
-    }
-    document.querySelector('#display h1').textContent = number;
-}
-
 
 // concatenate number to display
 function concatDisplay(n) {
-    // clear if initial number is 0
-    if (getDisplayedNumber() === '0') {
+    // clear if initial number is 0 and it is not displaying a decimal
+    if (getDisplayedNumber() === '0' && !getDisplayingFloat()) {
         clearDisplay();
     }
     // add number to end of display
@@ -189,7 +173,62 @@ function concatDisplay(n) {
 
 // clear display
 function clearDisplay() {
+    // cleared display no longer has float value
+    setDisplayingFloat(false);
+    // remove everything from display
     setDisplayedNumber('');
+}
+
+// GETTERS AND SETTERS //
+
+// set stored number in calculator object
+function setStoredNumber(n) {
+    calculator.stored = n;
+}
+
+function getStoredNumber() {
+    return calculator.stored;
+}
+
+// get string representation of number currently displayed
+function getDisplayedNumber() {
+    // return  number, multiplying by -1 if negative sign is displayed
+    const number = getDisplayTextNode().textContent
+    // store every digit after and including decimal
+    const splitOnDecimal = String(number).split('.')
+    const decimalAndTrailingDigits = splitOnDecimal.length === 2 ? '.' + splitOnDecimal[1] : '';
+    const digitsBeforeDecimal = splitOnDecimal[0]
+    const stringNegated = String(digitsBeforeDecimal * (displayingNegative() ? -1 : 1) + decimalAndTrailingDigits);
+    // return string with '.' at end if there was a point at the end to begin with
+    return stringNegated;
+}
+
+// set number to be displayed on calculator
+function setDisplayedNumber(number) {
+    // ensure input number is a string
+    number = String(number);
+    console.log('setting display to ' + number);
+    // if the number is negative, display negative sign and absolute value
+    const negativeSpan = document.querySelector('#display span')
+    if (+number < 0) {
+        negativeSpan.textContent = '-';
+    } else {
+        negativeSpan.textContent = '';
+    }
+    // // if last 'digit' is a point (.) indicate that it should be appended back on to the end
+    // const endsWithPoint = (String(number).at(-1) === '.');
+    // store every digit after and including decimal
+    const splitOnDecimal = String(number).split('.')
+    const decimalAndTrailingDigits = splitOnDecimal.length === 2 ? '.' + splitOnDecimal[1] : '';
+    const digitsBeforeDecimal = splitOnDecimal[0]
+    // display error if not a number, otherwise display absolute value
+    if (isNaN(+number)) {
+        number = 'ERROR';
+    } else {
+        // get the absolute value of the integer and then append trailing digits
+        number = Math.abs(digitsBeforeDecimal) + decimalAndTrailingDigits;
+    }
+    getDisplayTextNode().textContent = number;
 }
 
 // get currently selected operation
@@ -212,6 +251,7 @@ function setClearOnEntry(bool) {
     calculator.clearOnEntry = bool;
 }
 
+// return whether '-' is on calculator display
 function displayingNegative() {
     return document.querySelector('#display span').textContent === '-';
 }
@@ -236,24 +276,74 @@ function getEqualCanRepeat() {
     return calculator.equalCanRepeat;
 }
 
-// set positive to negative and vice versa
-function negateDisplayedNumber() {
-    setDisplayedNumber(+getDisplayedNumber() * -1);
+// return the last character currently displayed
+function getLastCharOnDisplay() {
+    return getDisplayTextNode().textContent.at(-1);
 }
 
-function handleEntry(number) {
+// get node for displayed text
+function getDisplayTextNode() {
+    return document.querySelector('#display h1');
+}
 
+// set positive to negative and vice versa
+function negateDisplayedNumber() {
+    // if last 'digit' is a point (.) indicate that it should be appended back on to the end
+    const endsWithPoint = (getLastCharOnDisplay() === '.');
+    const numberString = getDisplayedNumber();
+    // convert string to number and back to string then multiply by negative 1
+    let negativeNumber = String(+numberString * -1);
+    // add back '.' at the end if there was one to begin with
+    negativeNumber = endsWithPoint ? negativeNumber + '.' : negativeNumber;
+    setDisplayedNumber(negativeNumber);
+}
+
+// store whether there is a decimal point on display
+function setDisplayingFloat(bool) {
+    calculator.displayingFloat = bool;
+}
+
+// recall whether there is a decimal point
+function getDisplayingFloat() {
+    return calculator.displayingFloat;
 }
 
 // reset entire calculator
 function resetCalculator() {
     clearDisplay();
+    // run all setters
     setStoredNumber(NaN);
     setCurrentOperation('');
     setClearOnEntry(false);
     setDisplayedNumber(0);
     setLastEval(null);
     setEqualCanRepeat(false);
+}
+
+// MATHEMATICAL OPERATIONS //
+
+// add
+function add(a, b) {
+    return (a + b);
+}
+// subtract
+function subtract(a, b) {
+    return a - b;
+}
+// multiply
+function multiply(a, b) {
+    return a * b;
+}
+// divide
+function divide(a, b) {
+    return a / b;
+}
+
+// return how many times to multiply by 10 to get an integer from the float
+function digitsAfterDecimal(n) {
+    const splitString = String(n).split('.');
+    // return how many digits are after the float
+    return splitString.length === 2 ? splitString[1].length : 0;
 }
 
 
